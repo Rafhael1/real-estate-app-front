338e067d1dd365d097a1772421a003c8ca7ce410
Rafhael <rafamlf.2011@gmail.com>
2021-10-15 21:27:18 -0300

    small changes

--- src/pages/User/Dashboard/index.tsx
+++ src/pages/User/Dashboard/index.tsx
@@ -2,39 +2,7 @@ import React from 'react'
 
 import NewPropertyForm from './NewPropertyForm'
 
-const Admin = (): JSX.Element => {
-
-	// // States
-	// const [ file1, setFile1 ]: any = useState(null)
-
-
-	// const [ title, setTitle ] = useState("Cabana")
-	// const [ description, setDescription ] = useState("Not description")
-	// const [ address, setAddress ] = useState("A address placeholder")
-	// const [ country, setCountry ] = useState("Country")
-	// const [ price, setPrice ] = useState("1234124")
-	// const [ status, setStatus ] = useState("Sold out")
-    
-	// const api = async (e: any) => {
-	// 	e.preventDefault()
-	// 	try {
-	// 		const formData: any = new FormData()
-	// 		formData.append("image", file1, file1.name)
-	// 		//formData.append('document', json);
-	// 		//formData.append('document', blob);
-    
-	// 		const res = await axios.post("/upload", formData, {
-	// 			headers: {
-	// 				"Content-Type": "multipart/form-data",
-	// 			},
-	// 		}) 
-    
-	// 		console.log(res)
-    
-	// 	} catch (error) {
-	// 		console.log(error)
-	// 	}
-	// }
+const Admin = () => {
 
 	return (
 		<div>
