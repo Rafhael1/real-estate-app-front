8815ddf5a88070f52d65abd23d212e2468fe6278
Rafhael <rafamlf.2011@gmail.com>
2021-12-07 21:54:23 -0300

    cypress and prettier

---
+++ cypress/integration/Common/Login.spec.ts
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe('Login Screen', () => {
+    it('Should log in', () => {
+        cy.visit('/login')
+
+        cy.get(':nth-child(1) > .MuiOutlinedInput-root > .MuiOutlinedInput-input')
+        .type('oi')
+    })
+})
