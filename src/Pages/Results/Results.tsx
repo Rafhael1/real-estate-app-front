b69828b493db2ea60899f47f4224f7b5cedb92ff
Rafhael Marques <rafhaelmarques@protonmail.com>
2022-08-20 23:40:55 -0300

    ident

--- src/Pages/Results/Results.tsx
+++ src/Pages/Results/Results.tsx
@@ -7,56 +7,56 @@ import { getSearchResults } from 'Services/Search/Search.action';
 import useMediaQueryHook from 'Utils/Hooks/useMediaQuery';
 
 const Results = () => {
-  const dispatch = useDispatch();
-  // const navigate = useNavigate();
-  const { isMobile } = useMediaQueryHook();
+	const dispatch = useDispatch();
+	// const navigate = useNavigate();
+	const { isMobile } = useMediaQueryHook();
 
-  const searchSlice = useSelector((state) => state.Search);
+	const searchSlice = useSelector((state) => state.Search);
 
-  const handlePageChange = (event, page: number) => {
-    dispatch(
-      getSearchResults({
-        ...searchSlice.form,
-        page
-      })
-    );
-  };
+	const handlePageChange = (event, page: number) => {
+		dispatch(
+			getSearchResults({
+				...searchSlice.form,
+				page
+			})
+		);
+	};
 
-  useEffect(() => {
-    if (searchSlice.hasRequested) {
-      dispatch(getSearchResults(null));
-    }
-  }, []);
+	useEffect(() => {
+		if (searchSlice.hasRequested) {
+			dispatch(getSearchResults(null));
+		}
+	}, []);
 
-  return (
-    <Box>
-      <Navbar />
-      <Container>
-        <Box marginTop={5} textAlign="center">
-          <SearchBar />
-        </Box>
-        <Grid container spacing={0} sx={{ margin: '15px auto', gap: 2 }}>
-          {searchSlice.posts.map((post, index) => (
-            <Grid item xs={12} key={index}>
-              <PostPublic content={post} />
-            </Grid>
-          ))}
-        </Grid>
-        <Box>
-          {searchSlice.pagination.totalPages ? (
-            <Pagination
-              count={searchSlice.pagination.totalPages}
-              shape="rounded"
-              size="large"
-              color="primary"
-              sx={{ marginLeft: isMobile ? '30%' : '50%' }}
-              onChange={handlePageChange}
-            />
-          ) : null}
-        </Box>
-      </Container>
-      <Footer />
-    </Box>
-  );
+	return (
+		<Box>
+			<Navbar />
+			<Container>
+				<Box marginTop={5} textAlign="center">
+					<SearchBar />
+				</Box>
+				<Grid container spacing={0} sx={{ margin: '15px auto', gap: 2 }}>
+					{searchSlice.posts.map((post, index) => (
+						<Grid item xs={12} key={index}>
+							<PostPublic content={post} />
+						</Grid>
+					))}
+				</Grid>
+				<Box>
+					{searchSlice.pagination.totalPages ? (
+						<Pagination
+							count={searchSlice.pagination.totalPages}
+							shape="rounded"
+							size="large"
+							color="primary"
+							sx={{ marginLeft: isMobile ? '30%' : '50%' }}
+							onChange={handlePageChange}
+						/>
+					) : null}
+				</Box>
+			</Container>
+			<Footer />
+		</Box>
+	);
 };
 export default Results;
