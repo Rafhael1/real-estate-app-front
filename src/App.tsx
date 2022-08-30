fe4180a425accbe7114b4273a88d58f50af5f218
Rafhael Marques <rafhaelmarques@protonmail.com>
2022-08-30 20:16:59 -0300

    loading suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import Routes from './Pages/Router';
 import Snackbar from 'Components/Snackbar/Snackbar';
-import { Footer, Navbar } from 'Components';
+import LoadingSuspense from 'Components/Layout/LoadingSuspense/LoadingSuspense';
+
+const Navbar = React.lazy(() => import('Components/Layout/Navbar/Navbar'));
+const Footer = React.lazy(() => import('Components/Layout/Footer/Footer'));
+const Routes = React.lazy(() => import('Pages/Router'));
 
 const App = () => {
 	return (
-		<>
+		<Suspense fallback={<LoadingSuspense />}>
 			<Router>
 				<Snackbar />
 				<Navbar />
 				<Routes />
 				<Footer />
 			</Router>
-		</>
+		</Suspense>
 	);
 };
 
